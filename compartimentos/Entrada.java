2d0e5963bf713f666827d6f207cb4a2ad9f4d5ee
Laryssa <laryssasousa@aluno.uema.br>
2020-11-19 23:20:53 -0300

    Entrada e Saida

--- compartimentos/Entrada.java
+++ compartimentos/Entrada.java
@@ -4,10 +4,46 @@ import models.moveis.*;
 import interfaces.*;
 import java.util.ArrayList;
 
-public class Entrada{
+public class Entrada implements Status, FuncionamentoPassagem {
 
-    public Entrada(){
+    private ArrayList<Cliente> filaEntrada = new ArrayList<>();
 
+    public Entrada() {
+        existir();
+    }
+
+    //getters setters
+    public ArrayList<Cliente> getFilaEntrada() {
+        return filaEntrada;
+    }
+
+    public void setFilaEntrada(ArrayList<Cliente> filaEntrada) {
+        this.filaEntrada = filaEntrada;
+    }
+    
+    @Override
+    public void existir() {
+        System.out.println("Entrada criada");
+    }
+    
+    @Override
+    public int exibirStatus() {
+        int quantidade = filaEntrada.size();
+        System.out.println("Existem " + quantidade + " pessoas na fila esperando vaga no salão");
+        return quantidade;  
+    }
+
+    @Override
+    public void entrar(Cliente cliente) {
+        filaEntrada.add(cliente);
+    }
+
+    @Override
+    public Pessoa sair() {
+        Cliente clienteTemp = filaEntrada.get(0);
+        filaEntrada.remove(0);
+        return (Pessoa)clienteTemp;
     }
     
 }
+
--- compartimentos/Saida.java
+++ compartimentos/Saida.java
@@ -1,13 +1,49 @@
 package compartimentos;
 
-import models.moveis.*;
 import interfaces.*;
+import models.moveis.*;
 import java.util.ArrayList;
 
-public class Saida{
+public class Saida implements Status, FuncionamentoPassagem {
+
+    private ArrayList<Pessoa> filaSaida = new ArrayList<>();
 
     public Saida(){
-        
+        existir();
     }
+
+    //getters setters
+    public ArrayList<Pessoa> getFilaSaida() {
+        return filaSaida;
+    }
+
+    public void setFilaSaida(ArrayList<Pessoa> filaSaida) {
+        this.filaSaida = filaSaida;
+    }
+
+    @Override
+    public void existir() {
+        System.out.println("Saída criada");
+    }
+
+    @Override
+    public void entrar(Cliente cliente) {
+        filaSaida.add(cliente);
+    }
+
+    @Override
+    public Pessoa sair() {
+        Pessoa clienteTemp = filaSaida.get(0);
+        filaSaida.remove(0);
+        return clienteTemp;
+    }
+
+    @Override
+    public int exibirStatus() {
+        int quantidade = filaSaida.size();
+        System.out.println("Existem " + quantidade + " pessoas saindo do restaurante");
+        return quantidade;
+    }
+
     
 }
